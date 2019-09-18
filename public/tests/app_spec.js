13604d539ad7a37f4bde19a3eff0e5df115f96ac
Yohei Hasegawa <hasegawa.yohei@a-tm.co.jp>
2019-09-18 18:10:05 +0900

    3.3の最初のテスト作成まで実施
    @もくもく会

--- public/tests/app_spec.js
+++ public/tests/app_spec.js
@@ -16,18 +16,20 @@ describe('LearnJS', function() {
   });
 
   describe('problem view', function(){
+    var view;
+    beforeEach(function() {
+      view = learnjs.problemView('1');
+    });
+
     it('has a title that includes the problem number', function(){
-      var view = learnjs.problemView('1');
       expect(view.find('.title').text()).toEqual('Problem #1');
     });
 
     it('shows the description', function() {
-      var view = learnjs.problemView('1');
       expect(view.find('[data-name="description"]').text()).toEqual('What is truth?');  
     });
 
     it('shows the problem code', function(){
-      var view = learnjs.problemView('1');
       expect(view.find('[data-name="code"]').text()).toEqual('function problem() { return __; }');
     });
 
@@ -42,6 +44,20 @@ describe('LearnJS', function() {
       spyOn(learnjs, 'showView');
       $(window).trigger('hashchange');
       expect(learnjs.showView).toHaveBeenCalledWith(window.location.hash);
+    });
+  });
+
+  describe('answer section', function(){
+    it('can check a correct answer by hitting a button', function(){
+      view.find('.answer').val('true');
+      view.find('.check-btn').click();
+      expect(view.find('.result').text()).toEqual('Correct!');
+    });
+
+    it('rejects an incorrect answer', function(){
+      view.find('.answer').val('false');
+      view.find('.check-btn').click();
+      expect(view.find('.result').text()).toEqual('Incorrect!');
     });
   });
  });
